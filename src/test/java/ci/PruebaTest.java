b9818863f7e5d0dd910de1ce0d0cf464e016a2f8
Jesús Sánchez <jesusc@um.es>
2026-04-13 12:52:36 +0200

    Ejemplo

---
+++ src/test/java/ci/PruebaTest.java
@@ -0,0 +1,14 @@
+package ci;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PruebaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
